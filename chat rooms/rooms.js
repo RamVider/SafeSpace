3de1a218c2f71b70aaffcc5ee2e962af594853f1
yehuda748 <yehuda22@zvialod.com>
2022-06-16 17:34:24 +0300

    chatRooms first version

--- chat rooms/rooms.js
+++ chat rooms/rooms.js
@@ -4,8 +4,10 @@ let rooms = ["grop 1", "prop 2", "prop 3", "prop 4"];
 
     for (let i = 0; i < users.length; i++) {
         let div = `<div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 nameSlot">
-                        ${users[i]}
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+                        <div class="nameSlot">
+                            ${users[i]}
+                        </div>
                     </div>
                 </div>`;
         document.getElementById("usersContainer").innerHTML += div;
@@ -14,7 +16,7 @@ let rooms = ["grop 1", "prop 2", "prop 3", "prop 4"];
 
 for (let i = 0; i < rooms.length; i++) {
     let div = `
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 roomSlot ">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 roomSlot" >
                     ${rooms[i]}
                 </div>`;
     document.getElementById("roomsContainer").innerHTML += div;
