a1c79818759451ba2f57a47237f3cc1cc3f874d7
RamVider <ramvider@gmail.com>
2022-06-19 14:12:45 +0300

    uploading server

--- server/server123.js
+++ server/server123.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+filePath = "/db/db.txt";
 const express = require('express')
 const cors = require('cors')
 const res = require('express/lib/response')
@@ -7,44 +9,26 @@ const { json } = require('express/lib/response');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(cors())
-const fs = require('fs')
-filePath = "/db/db.txt";
 
 app.listen(3000)
 app.post('/', function (req, res) {
+
     let users = [];
     let db = readFromFile()
-    let result = addUsersToDB(db, users, req.body)
+    if (db!==""){   
+        db = JSON.parse(db)
+        if (typeof db == "object" && db.length > 0) {
+            users = db;
+        }
+    }
     console.log(users)
-    res.send(result)
-});
-
-function addUsersToDB(db, users, body) {
-    if (db !== "") {
-        users = JSON.parse(db)
-        if (isUserExist(body, users)) {
-            return "user exist";
-        }
-    }
-    users.push(body);
+    users.push(req.body)
     saveToFile(JSON.stringify(users))
-    return "user added"
-}
 
-function isUserExist(newUser, users) {
-    let result = false;
-    if (typeof users == "object" && users.length > 0) {
-        let resUser = users.find(function (user) {
-            return user.email === newUser.email
-        })
-        if (resUser !== undefined) {
-            result = true
-        }
-    }
-    return result
-}
+})
 
 app.get('/', function (req, res) {
+
 
 
 })
