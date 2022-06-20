058d09108294c857452e013b0fec5c06707c95ab
yehuda748 <yehuda22@zvialod.com>
2022-06-20 19:41:57 +0300

    db

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -1,5 +1,6 @@
 function openForm() {
   document.getElementById("myForm").style.display = "block";
+  // window.location.href="C:/Users/yehud/Documents/project/SaveSpace/chatRooms/chats.html"
 }
   
 function closeForm() {
