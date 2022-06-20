d123903b797e173533e4a194888c836adb4202f7
RamVider <ramvider@gmail.com>
2022-06-20 19:57:34 +0300

    make header dynamic

---
+++ commonFiles/header.js
@@ -0,0 +1,27 @@
+$(function () {
+  $("#header").html(headerHTML);
+});
+let headerHTML = `<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">WebSiteName</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="file:///C:/Users/yehud/Documents/%D7%A7%D7%95%D7%A8%D7%A1%20%D7%9E%D7%97%D7%A9%D7%91%D7%94%20%D7%98%D7%95%D7%91%D7%94/home%20work/front%20Project/homePage/FG.html">Home</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>`;
--- homePage/homePage.js
+++ homePage/homePage.js
@@ -1,16 +1,17 @@
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
-  
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-function sendNum(){
-  let phone= $("#phone").val()
+function sendNum() {
+  let phone = $("#phone").val()
   $.post(`http://localhost:3000/`, phone, function (data, status) {
     if (status === "success") {
-        console.log("god")
+      console.log("god")
     }
   });
 }
