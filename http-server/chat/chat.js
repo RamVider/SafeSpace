b67df4b731f3a5b0eef99a1ade350af87baae30a
yehuda748 <yehuda22@zvialod.com>
2022-06-27 10:21:20 +0300

    add theMeseges evry sec

--- http-server/chat/chat.js
+++ http-server/chat/chat.js
@@ -1,10 +1,10 @@
 function sendMessage() {
     if ($("#input").val()) {
-        let input = $("#input").val() 
+        let input = $("#input").val()
         let user = $("#uName").text()
-        let data={
-            "user":user,
-            "input":input,
+        let data = {
+            "user": user,
+            "input": input,
         }
         $.post("http://localhost:3000/takeDataFromChat", data, function (data, status) { })
         $("#input").val("");
@@ -18,3 +18,27 @@ $(function () {
         }
     });
 });
+
+setInterval(function () {
+    $.get("http://localhost:3000/dataToChat", function (data, status) {
+        if (status === "success") {
+            // console.log(JSON.parse(data))
+            let message = JSON.parse(data)
+            let messageCOntainer=""
+            for (let i = 0; i < message.length; i++) {
+                let div=`
+                    <div>
+                        <p class="uNameTitel">${message[i].user}</p>
+                        <h3>${message[i].input}</h3>
+                    </div>
+                    </br>
+                    </br>
+                    </br>
+                `
+                messageCOntainer+=div
+            }
+            console.log(messageCOntainer)
+            $("#messages").html(messageCOntainer)
+        }
+    })
+}, 2000);
