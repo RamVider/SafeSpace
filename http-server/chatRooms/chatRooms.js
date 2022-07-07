4027a4e50b36ea6c8817be8bac4f54a2b487ed61
yehuda748 <yehuda22@zvialod.com>
2022-07-07 15:00:39 +0300

    chage in chatR whith pop up page

--- http-server/chatRooms/chatRooms.js
+++ http-server/chatRooms/chatRooms.js
@@ -20,6 +20,7 @@ let rooms = [{
 
 var users = [];
 
+
 function gett() {
     $.get("http://localhost:3000/roomsToRoomsPage", function (data, status){
         
@@ -62,9 +63,40 @@ function creatRoomsInHtml(rooms) {
     }
     $("#roomsContainer").html(container)
 }
+// createMorRoom(){
+    
+// }
 
 function goToChat(link) {
 
 }
 
 gett()
+
+
+//pop up js
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
